8155b76df90370c773c0fa6a3d26409608c832f9
Edison Garcia Chinas <edisga@microsoft.com>
2020-06-20 15:13:30 -0500

    Adding settings for App Service Linux

--- rack-mini-profiler/config/initializers/mini_profiler.rb
+++ rack-mini-profiler/config/initializers/mini_profiler.rb
@@ -1,3 +1,5 @@
+
+
 # FileStore is by default and will write to tmp/miniprofiler/*
 
 # set MemoryStore
@@ -8,3 +10,9 @@
 #    Rack::MiniProfiler.config.storage_options = { url: ENV["REDIS_SERVER_URL"] }
 #    Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
 # end
+
+# Memory profiler is disabled by default in production, you can enable using this:
+# For more configuration options: https://github.com/MiniProfiler/rack-mini-profiler#configuration-options
+if Rails.env.production?
+    Rack::MiniProfiler.config.enable_advanced_debugging_tools = true
+end
